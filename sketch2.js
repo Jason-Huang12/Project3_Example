706b2a266e8fbae6e768539e452cbfcea41ce266
Jason-Huang12 <55153264+Jason-Huang12@users.noreply.github.com>
2019-10-10 11:51:49 -0700

    summary

---
+++ sketch2.js
@@ -0,0 +1,17 @@
+var img;
+function preload() {
+img = loadImage("Images/fireball-PNG-transparent-background-thumb35.png");
+}
+
+function setup() {
+createCanvas(480,480);
+}
+
+function draw() {
+  background(204);
+  fill(0,0,255);
+  rect(50,50,50,50);
+  fill(255,0,0);
+  rect(380,50,50,50);
+  image(img,250,250);
+}
